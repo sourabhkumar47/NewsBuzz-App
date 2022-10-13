172b7c9b98b31811636e01a60926beda174c9172
Sourabh Kumar <20010001030@gateway.edu.in>
2022-10-14 00:11:19 +0530

    Created homeScreen

--- app/src/main/java/com/example/newsbuzz/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newsbuzz/presentation/MainActivity.kt
@@ -6,12 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.newsbuzz.presentation.screens.HomeScreen
 import com.example.newsbuzz.ui.theme.NewsBuzzTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    NewsBuzz {
+                        HomeScreen()
+                    }
                 }
             }
         }
@@ -30,14 +33,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NewsBuzzTheme {
-        Greeting("Android")
-    }
+fun NewsBuzz(content: @Composable () -> Unit) {
+    content()
 }
--- app/src/main/java/com/example/newsbuzz/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/newsbuzz/presentation/screens/HomeScreen.kt
@@ -1,2 +1,63 @@
 package com.example.newsbuzz.presentation.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberImagePainter
+import com.example.newsbuzz.domain.model.Article
+import com.example.newsbuzz.presentation.viewmodel.NewsViewModel
+
+@Composable
+fun HomeScreen(viewModel: NewsViewModel = hiltViewModel()) {
+    val res = viewModel.articles.value
+
+    if (res.isLoading) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+
+    if (res.error.isNotBlank()) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(text = res.error, modifier = Modifier.align(Alignment.Center))
+        }
+    }
+
+    res.data?.let {
+        LazyColumn {
+            item(it) {
+                ArticleItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ArticleItem(it: Article) {
+    Column(modifier = Modifier) {
+
+        Image(
+            painter = rememberImagePainter(data = it.urlToImage),
+            contentDescription = null,
+            modifier = Modifier
+                .height(300.dp)
+                .fillMaxSize()
+        )
+
+        Text(
+            text = it.title,
+            style = TextStyle(fontWeight = FontWeight.SemiBold),
+            modifier = Modifier.padding(12.dp)
+        )
+
+    }
+}

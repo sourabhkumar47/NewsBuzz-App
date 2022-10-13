c2297b23b8d52324765e6c7095086958f8e8c01f
Sourabh Kumar <20010001030@gateway.edu.in>
2022-10-13 23:35:03 +0530

    Completed viewModel

--- app/src/main/java/com/example/newsbuzz/presentation/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/newsbuzz/presentation/viewmodel/NewsViewModel.kt
@@ -1,13 +1,40 @@
 package com.example.newsbuzz.presentation.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.newsbuzz.domain.use_case.GetNewsArticleUseCase
+import com.example.newsbuzz.presentation.HomeStateHolder
+import com.example.newsbuzz.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
 class NewsViewModel @Inject constructor(private val getNewsArticleUseCase: GetNewsArticleUseCase) :
     ViewModel() {
-    
-        val articles = mutableSetOf()
+
+    val articles = mutableStateOf(HomeStateHolder())
+
+    init {
+        getNewsArticle()
+    }
+
+    fun getNewsArticle() {
+        getNewsArticleUseCase().onEach {
+            when (it) {
+                is Resource.Loading -> {
+                    articles.value = HomeStateHolder(isLoading = true)
+                }
+                is Resource.Success -> {
+                    articles.value = HomeStateHolder(data = it.data)
+                }
+                is Resource.Error -> {
+                    articles.value = HomeStateHolder(error = it.message.toString())
+                }
+            }
+
+        }.launchIn(viewModelScope)
+    }
 }

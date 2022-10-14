afc92a1669c74b2defaa78dffc930e2891b01bbb
Sourabh Kumar <20010001030@gateway.edu.in>
2022-10-14 23:37:20 +0530

    Fixed size

--- app/src/main/java/com/example/newsbuzz/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/newsbuzz/presentation/screens/HomeScreen.kt
@@ -5,14 +5,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -54,83 +55,79 @@ fun HomeScreen(viewModel: NewsViewModel = hiltViewModel()) {
         }
 
     }
-
-
 }
 
 @Composable
 fun ArticleItem(it: Article) {
-
-    Column(
-        modifier = Modifier
-            .padding(34.dp)
-    ) {
-
-        Image(
-            painter = rememberImagePainter(data = it.urlToImage),
-            contentDescription = null,
+    Surface {
+        Column(
             modifier = Modifier
-                .height(208.dp)
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(24.dp))
-
-        )
-
-        Spacer(modifier = Modifier.height(12.dp))
-
-        Text(
-            text = it.title, style = TextStyle(
-                color = Color.Gray,
-                fontWeight = FontWeight.Bold,
-                fontSize = 22.sp
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = rememberImagePainter(data = it.urlToImage),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(208.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(24.dp))
             )
-//            modifier = Modifier.padding(12.dp)
-        )
 
-        Spacer(modifier = Modifier.height(5.dp))
+            Spacer(modifier = Modifier.height(1.dp))
 
-        Text(
-            text = it.description,
-            style = TextStyle(
-                fontWeight = FontWeight.Normal,
-                fontSize = 17.sp
-            )
-        )
-
-        Spacer(modifier = Modifier.height(12.dp))
-
-        it.publishedAt?.let { it1 ->
             Text(
-                text = it.publishedAt,
+                text = it.title,
                 style = TextStyle(
-                    color = Color.Gray,
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
                 )
             )
-        }
 
-        Spacer(modifier = Modifier.height(12.dp))
+            Spacer(modifier = Modifier.height(1.dp))
 
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(10.dp),
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-        ) {
-            Button(onClick = { },
-                modifier = Modifier
-                    .clip(RoundedCornerShape(16.dp))
-            ) {
-                Text(text = "Read")
+            Text(
+                text = it.description,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 15.sp
+                )
+            )
+
+            Spacer(modifier = Modifier.height(5.dp))
+
+            it.publishedAt?.let { it1 ->
+                Text(
+                    text = it.publishedAt,
+                    style = TextStyle(
+                        color = Color.Gray,
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 12.sp,
+                    )
+                )
             }
 
-            Button(onClick = { },
+            Spacer(modifier = Modifier.height(1.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
                 modifier = Modifier
-                    .clip(RoundedCornerShape(16.dp))
+                    .align(Alignment.CenterHorizontally)
             ) {
-                Text(text = "Save")
+                Button(
+                    onClick = { },
+                    shape = RoundedCornerShape(50.dp),
+                ) {
+                    Text(text = "Read")
+                }
+
+                Button(
+                    onClick = { },
+                    shape = RoundedCornerShape(50.dp)
+                ) {
+                    Text(text = "Save")
+                }
             }
         }
-
     }
+
 }

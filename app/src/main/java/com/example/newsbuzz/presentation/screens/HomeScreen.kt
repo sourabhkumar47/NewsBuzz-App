4e1765563c9617b90acf9c13d590f076df54a4fd
Sourabh Kumar <20010001030@gateway.edu.in>
2022-10-15 00:20:47 +0530

    Fixed size

--- app/src/main/java/com/example/newsbuzz/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/newsbuzz/presentation/screens/HomeScreen.kt
@@ -6,17 +6,21 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberImagePainter
+import com.example.newsbuzz.R
 import com.example.newsbuzz.domain.model.Article
 import com.example.newsbuzz.presentation.viewmodel.NewsViewModel
 
@@ -43,10 +47,11 @@ fun HomeScreen(viewModel: NewsViewModel = hiltViewModel()) {
 
     }
 
-
+    header()
     res.data?.let {
         LazyColumn {
             items(it) {
+
                 ArticleItem(it)
             }
         }
@@ -126,5 +131,25 @@ fun ArticleItem(it: Article) {
             }
         }
     }
+}
 
+@Composable
+fun header() {
+
+    Row(modifier = Modifier
+        .padding(16.dp)) {
+
+        Box {
+            Image(
+                painterResource(id = R.drawable.header_image),
+                contentDescription = null
+            )
+            Icon(
+                imageVector = Icons.Default.Bookmark,
+                contentDescription = null,
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+            )
+        }
+    }
 }
